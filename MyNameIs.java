0566af0e45ba30e2a7850915ec3ecfba12a02499
Lauren Rydh <lauren_ya@live.com>
2023-05-08 14:56:58 -0500

    Finished Java basic printing practice

---
+++ MyNameIs.java
@@ -0,0 +1,23 @@
+public class MyNameIs {
+    public static void main(String[] args) {
+       System.out.println("My name is");
+       System.out.println("Larry");
+       System.out.println("Sprinkle");
+    }
+}
+
+class Vacation { 
+    public static void main(String[] args) {
+        System.out.println("I'd like to visit");
+        System.out.println("Paris");
+        System.out.println("France");
+    }
+}
+
+class HiFriend {
+    public static void main(String[] args) {
+        String name = "Larry Sprinkle";
+        System.out.println("Hi " + name + "!");
+
+    }
+}
